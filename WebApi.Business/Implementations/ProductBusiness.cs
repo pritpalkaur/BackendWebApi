3be85a7be4758a0838214d13eb8c9e0da5a52d4b
pritpalkaur <pritpalkaur05@gmail.com>
2024-08-19 20:23:07 +0800

    pushing latest

---
+++ WebApi.Business/Implementations/ProductBusiness.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Business;
+using WebApi.Business.Interface;
+
+
+namespace WebApi.Business.Implementations
+{
+    public class ProductBusiness : IProductBusiness 
+    {
+        //private readonly IProductDatabase;
+        public ProductBusiness()
+        {
+                
+        }
+
+    }
+}
