ad62868e75d8295753d48664a31b65aa59bc4a45
pritpalkaur <pritpalkaur05@gmail.com>
2024-08-19 13:17:46 +0800

    required file

---
+++ WebApi.Business/Implementations/ProjectBusiness.cs
@@ -0,0 +1,26 @@
+﻿//using Microsoft.EntityFrameworkCore;
+//using mMoser.Data.Helpers;
+//using mMoser.Data;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Linq.Dynamic.Core;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using mMoser.Business.Interface;
+
+
+namespace mMoser.Data
+{
+    public class ProjectBusiness : IProjectBusiness
+    {
+
+        //private readonly IProjectDatabase _iProjectDatabase;
+        //public ProjectBusiness(IProjectDatabase iProjectDatabase)
+        //{
+        //    this._iProjectDatabase = iProjectDatabase;
+        //}
+
+
+      
+    }
+}
